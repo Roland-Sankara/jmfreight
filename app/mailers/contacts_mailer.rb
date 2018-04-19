b870c0595d98fd9066e8840f8cbc5c8e7fd4a87e
alex shyaka <shyakaster@gmail.com>
2018-04-19 05:32:46 +0300

    change email to forward to for contact form

--- app/mailers/contacts_mailer.rb
+++ app/mailers/contacts_mailer.rb
@@ -4,6 +4,6 @@ class ContactsMailer < ApplicationMailer
 
 	def send_email(email)
 	    @email = email
-	    mail(to: "nkusialex@gmail.com", subject: email.subject)
+	    mail(to: "john@jmfreight.co.ug", subject: email.subject)
 	end
 end
